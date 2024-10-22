99c0ac7034d55eb6a4b9951203344d8f0e59b432
Linh <linh@gami.com>
2024-10-23 01:10:33 +0700

    final

--- Ex11/app/src/main/java/com/example/ex11/MainActivity.java
+++ Ex11/app/src/main/java/com/example/ex11/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_show;
+    EditText et_link;
+    TextView tv_title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btn_show = findViewById(R.id.btn_show);
+        et_link = findViewById(R.id.et_link);
+        tv_title = findViewById(R.id.tv_title);
+        btn_show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(et_link.getText().toString()));
+                startActivity(intent);
+            }
         });
     }
 }
